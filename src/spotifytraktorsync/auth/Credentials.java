fcaf5d489474f356b313ec4ad8c63a223e4846bf
band-ag <104816534+band-ag@users.noreply.github.com>
2022-05-04 16:27:51 +0200

    Update Credentials.java

--- src/spotifytraktorsync/auth/Credentials.java
+++ src/spotifytraktorsync/auth/Credentials.java
@@ -8,8 +8,8 @@ import java.util.Base64;
 public class Credentials {
 
     public static String
-        CLIENT_ID = "7958fce2169348c9bb75ede05e1f12c2",
-        CLIENT_SECRET = "ffcb0b86ed004735b963d4bcd51002cf";
+        CLIENT_ID = "",
+        CLIENT_SECRET = "";
 
     public static String AUTHORIZATION = Base64.getEncoder().encodeToString((CLIENT_ID + ":" + CLIENT_SECRET).getBytes(StandardCharsets.UTF_8));
     public static String ACCESS_TOKEN = "null";
